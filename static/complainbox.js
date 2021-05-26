ae69822dcb874ed7d1afc22116b0e8f3974f73dc
Ash515 <aaashwin515@gmail.com>
2021-05-26 20:51:30 +0530

    added new files

--- static/complainbox.js
+++ static/complainbox.js
@@ -3,7 +3,10 @@ function listclick(){
     document.getElementById('lm');
     document.querySelector('.about-model').style.display="flex";
 }
-
+function saveclose(){
+  document.querySelector('.comp-reply').style.display='none';
+  document.querySelector('#profile-info').style.display='block';
+}
 function reply(){
   document.querySelector('.comp-reply').style.display='block';
   document.querySelector('#profile-info').style.display='none';
